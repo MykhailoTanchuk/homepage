6a7b08a7285bc60308d12a63f3fa60e557e9903f
Mike Tanchuk <volkermischa@gmail.com>
2023-06-18 16:09:10 +0300

    Portfolio localized

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import projects from '../portfolio/projects.js';
+import {useTranslation} from "react-i18next";
 const Portfolio = () => {
+
+    const {t, i18n} = useTranslation();
 
     return (
         <div id={`portfolio`} className="p-4 md:p-8">
-            <h2 className="text-2xl font-bold mb-6">Portfolio</h2>
+            <h2 className="text-2xl font-bold mb-6">{t(`portfolio_title`)}</h2>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {projects.map((project, index) => (
@@ -12,12 +15,12 @@ const Portfolio = () => {
                         <img className="h-64 w-full object-cover" src={project.image} alt={project.title}/>
                         <div className="p-6">
                             <h3 className="text-xl font-bold mb-2">{project.title}</h3>
-                            <p className="text-gray-700 mb-4">{project.description}</p>
+                            <p className="text-gray-700 mb-4">{i18n.language === "en" ? project.description : project.descriptionUA}</p>
                             <div className="mt-auto">
                                 <a href={project.githubUrl} target="_blank" rel="noopener noreferrer" className="btn m-2">GitHub</a>
                                 <a href={project.websiteUrl} target="_blank" rel="noopener noreferrer"
                                    className={`btn m-2 ${project.isDisabled ? 
-                                       `bg-red-600 pointer-events-none` : ''}`}>Website</a>
+                                       `bg-red-600 pointer-events-none` : ''}`}>{t(`website`)}</a>
                             </div>
                         </div>
                     </div>
